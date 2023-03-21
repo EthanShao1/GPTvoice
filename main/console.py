38ac53854664a55943c208ea1ab46d71d9f2aa60
EthanShao1 <goduck92@gmail.com>
2023-03-21 19:37:57 +0800

    Initial commit

---
+++ main/console.py
@@ -0,0 +1,33 @@
+import os
+import speech_synthesis as speech
+from uuid import uuid4
+from gpt3contextual import ContextualChatGPT, ContextManager
+
+context_key = str(uuid4())
+
+with open("set.txt", mode= "r", encoding= "utf-8") as set:
+    setUp = set.read()
+
+async def response():
+    cm = ContextManager(
+        timeout = 9999999999,
+        username = "　你　",
+        agentname = "ひなた",
+        chat_description = setUp,
+    )
+    
+    cc = ContextualChatGPT(
+        os.environ.get("OPENAI_API"),
+        context_manager=cm
+    )
+    
+    while True:
+        text = input(f"{cm.username}> ")
+        resp, prompt, completion = await cc.chat(context_key, text)
+        print(f"{cm.agentname}> {resp}")
+        
+        data = completion["choices"][0]["message"]["content"]
+        with open("text.txt", mode= "w", encoding= "utf-8") as file:
+            file.write(data)
+
+        await speech.speaker()
---
+++ main/main.py
@@ -0,0 +1,6 @@
+import asyncio, console
+
+async def main():
+    await console.response(),
+
+asyncio.run(main())

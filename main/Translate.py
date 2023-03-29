c03ed54552996b370c9de58bd16d82b0dbc48624
EthanShao1 <goduck92@gmail.com>
2023-03-30 07:12:12 +0800

    將動作與對話分開(堪用)

--- main/Translate.py
+++ main/Translate.py
@@ -4,12 +4,16 @@ from translate import Translator
 async def translate_text():
     text = FO.fileRead("text/text.txt").strip("*").split("*")
     if len(text) > 1:
-        a = 1
+        for a in range(len(text)):
+            if (" " in text[a]) == False:
+                text.remove(text[a])
+        usetext = "".join(text)
     else:
-        a = 0
+        # usetext = str(text)
+        usetext = "".join(text)
 
     translator = Translator(from_lang= "zh-TW", to_lang= "ja")  # 設置目標語言
 
-    result = translator.translate(text[a])
+    result = translator.translate(usetext)
 
     FO.fileWrite("text/read.txt", result)

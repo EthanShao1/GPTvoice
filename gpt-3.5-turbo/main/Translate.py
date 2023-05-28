cbd316ec48ebd363a66232ac37ff82d2f25ee9ad
EthanShao1 <goduck92@gmail.com>
2023-05-29 01:15:17 +0800

    .

--- gpt-3.5-turbo/main/Translate.py
+++ gpt-3.5-turbo/main/Translate.py
@@ -4,15 +4,6 @@ from translate import Translator
 async def translate_text():
     text = FO.fileRead("text/text.txt").strip("*").split("*")
     if len(text) > 1:
-<<<<<<< HEAD
-        a = 1
-    else:
-        a = 0
-
-    translator = Translator(from_lang= "zh-TW", to_lang= "ja")  # 設置目標語言
-
-    result = translator.translate(text[a])
-=======
         for a in range(len(text)):
             if (" " in text[a]) == False:
                 text.remove(text[a])
@@ -24,6 +15,5 @@ async def translate_text():
     translator = Translator(from_lang= "zh-TW", to_lang= "ja")  # 設置目標語言
 
     result = translator.translate(usetext)
->>>>>>> 716af02f14a265ab14dd9a42e6067990eb3aadbb
 
     FO.fileWrite("text/read.txt", result)
--- gpt-3.5-turbo/test/fileOpen.py
+++
@@ -1,9 +0,0 @@
-def fileRead(fileAddress):
-    with open(fileAddress, mode= "r", encoding= "utf-8") as file:
-        data = file.read()
-    
-    return data
-
-def fileWrite(fileAddress, data):
-    with open(fileAddress, mode= "w", encoding= "utf-8") as file:
-        file.write(data)
